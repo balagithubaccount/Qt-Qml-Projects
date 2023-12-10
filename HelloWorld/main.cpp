94ea40eed4cd88088e040e178191d9e38f8e6a07
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-10 23:24:14 +0530

    qt console application.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     qInfo() << "Hellow World!.";
     qInfo() << "This is Console Application.";
+	qInfo() << "Qt Creator.";
     return a.exec();
 }
